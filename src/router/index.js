cee009e4dcbd00794b25fbe1667ced0644d3148b
Jose Luis <88117946+LuguiM@users.noreply.github.com>
2025-01-07 10:51:59 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     
     routes :[
         {
-            path:'/',
+            path:'/home',
             name: 'Home',
             component: () => import('../views/allCountries.vue')
         },
