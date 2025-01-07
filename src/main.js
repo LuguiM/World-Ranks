d30ec00ed76f39b044e68f89b24022e328fcc409
Jose Luis <88117946+LuguiM@users.noreply.github.com>
2025-01-07 10:25:53 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
-import 'boxicons'
+import 'boxicons/css/boxicons.min.css'
 
 const app = createApp(App)
 
