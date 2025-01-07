89e42377753cc3b5ebf3f74ad2c1887fd67152d3
Jose Luis <88117946+LuguiM@users.noreply.github.com>
2025-01-07 10:18:40 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
