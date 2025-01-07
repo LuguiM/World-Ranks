5577766e6980de68db80d174ed1bc354cd804288
Jose Luis <88117946+LuguiM@users.noreply.github.com>
2025-01-07 10:41:32 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
     // history: createWebHistory('https://luguim.github.io/World-Ranks/'),
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     
     routes :[
         {
